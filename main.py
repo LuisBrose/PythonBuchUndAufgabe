e7a91b091bd0c96c13cc6424b189493ebf375c79
Luis Brose <l.brose@intero-technologies.de>
2023-09-06 18:37:58 +0200

    bis Kapitel 4.5.4

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import fractions
+
 if __name__ == "__main__":
-    x = 1.8
-    for i in range(4):
-        print(x)
-        x = x + 0.1
+    x = fractions.Fraction(1.454165456432132)
+    print(x)
+    print(x.limit_denominator(1000))
+
+    y = "abdkfjgiucnglijngjol"
+    print(y[20:1:-1])
+
+    print(hash("abcde") % 10)
+
+    a, *b, c, d = 1, 2, 3, 4, 5, 6
+    print(a, b, c, d)
+
+    d = {"a": 1, "b": 1, "c": 1}
+    e = {"a": 2, "c": 2, "d": 2}
+    d.update(e)
+    print(d, d.keys(), d.items(), d.values())
